e45a67a857a05319b787184bba4f3949a0dc8365
jaikwangg <kanna9764@gmail.com>
2025-07-06 19:25:19 +0700

    fix config

--- vite-ui/vite.config.ts
+++ vite-ui/vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/aun-jai-care-connect/' : '/',
+  base: command === 'build' ? '/aun-jai-care-connect/' : '/',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, 'src'), // <--- ใส่ alias ให้ตรง
     },
   },
-})
+}))
