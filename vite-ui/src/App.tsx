295817702b41cd2effe43e9d6f1b208bb69d6cb3
jaikwangg <kanna9764@gmail.com>
2025-07-06 19:29:20 +0700

    fix router

--- vite-ui/src/App.tsx
+++ vite-ui/src/App.tsx
@@ -1,10 +1,10 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AppProvider } from "./contexts/AppContext";
+
 import Index from "./pages/Index";
 import Auth from "./pages/Auth";
 import SeniorDashboard from "./pages/SeniorDashboard";
@@ -23,31 +23,50 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+
+        <BrowserRouter basename="/aun-jai-care-connect">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/auth" element={<Auth />} />
-            <Route path="/senior-dashboard" element={
-              <ProtectedRoute requiredRole="senior">
-                <SeniorDashboard />
-              </ProtectedRoute>
-            } />
-            <Route path="/family-dashboard" element={
-              <ProtectedRoute requiredRole="family">
-                <FamilyDashboard />
-              </ProtectedRoute>
-            } />
-            <Route path="/book-care" element={
-              <ProtectedRoute>
-                <BookCare />
-              </ProtectedRoute>
-            } />
+
+            <Route
+              path="/senior-dashboard"
+              element={
+                <ProtectedRoute requiredRole="senior">
+                  <SeniorDashboard />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/family-dashboard"
+              element={
+                <ProtectedRoute requiredRole="family">
+                  <FamilyDashboard />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/book-care"
+              element={
+                <ProtectedRoute>
+                  <BookCare />
+                </ProtectedRoute>
+              }
+            />
+
             <Route path="/packages" element={<Packages />} />
-            <Route path="/profile" element={
-              <ProtectedRoute>
-                <Profile />
-              </ProtectedRoute>
-            } />
+
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
